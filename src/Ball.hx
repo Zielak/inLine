7b8ed727f804868b82366ca298d15db2c29c8c31
Darek Greenly <github@zielak.pl>
2014-12-26 21:06:52 +0100

    init

---
+++ src/Ball.hx
@@ -0,0 +1,110 @@
+
+package ;
+
+import luxe.Sprite;
+import luxe.Color;
+
+import phoenix.geometry.CircleGeometry;
+
+typedef BallOptions = {
+    var offset:Float;
+    @:optional var brmin:Float;
+    @:optional var brmax:Float;
+}
+
+class Ball extends Sprite
+{
+
+    public var baseY:Float;
+    public var inc:Float;
+    public var colinc:Float;
+    public var coldegree:Float;
+    public var brmin:Float;
+    public var brmax:Float;
+    public var degree:Float;
+    public var brvariation:Float;
+    public var yMag:Float;
+    public var minScale:Float;
+    public var maxScale:Float;
+    public var variation:Float;
+
+
+    var _geom:CircleGeometry;
+
+    var value:Float;
+    var value2:Float;
+    var brightness:Float;
+
+    public function generate(options:BallOptions):Void
+    {
+        baseY = Luxe.screen.h / 2;
+        inc = geninc(options.offset);
+        colinc = gencolinc(options.offset);
+        brmin = (options.brmin != null) ? options.brmin : 0;
+        brmax = (options.brmax != null) ? options.brmax : 40;
+        degree = coldegree = genstartDegree(options.offset);
+        brvariation = brmax - brmin;
+        yMag = genyMag(options.offset);
+        minScale = genminScale(options.offset);
+        maxScale = genmaxScale(options.offset);
+        variation = maxScale - minScale;
+
+        // geometry = new phoenix.geometry.CircleGeometry({
+        //     x : 0,
+        //     y : 0,
+        //     r : genminScale(),
+        //     color : new Color(0,1,0,1).rgb(0x00ff00)
+        // });
+
+    }
+
+    override public function update(dt:Float):Void
+    {
+        degree += inc;
+
+        value = Math.sin(degree * Math.PI/180);
+        size.y = minScale + variation/2 + (variation/2) * value;
+        size.x = 3;
+
+        coldegree += colinc;
+
+        value2 = Math.sin(coldegree * Math.PI/180);
+        brightness = brmin + brvariation/2 + (brvariation/2) * value2;
+
+        // setBrightness(color, brightness);
+        pos.y = baseY + value2 * yMag;
+    }
+
+    function setBrightness(color:Color, v2:Float):Void
+    {
+        
+    }
+
+
+
+    function geninc(val:Float):Float
+    {
+        return 3 * val;
+    }
+    function gencolinc(val:Float):Float
+    {
+        return 4;
+    }
+    function genyMag(val:Float):Float
+    {
+        return 3 * val;
+    }
+    static public function genminScale(val:Float = 0):Float
+    {
+        return 10;
+    }
+    static public function genmaxScale(val:Float = 0):Float
+    {
+        return 3 * val + 10;
+    }
+    function genstartDegree(val:Float):Float
+    {
+        return 2 * val;
+    }
+
+}
---
+++ src/Main.hx
@@ -0,0 +1,73 @@
+
+package ;
+
+import luxe.AppConfig;
+import luxe.Game;
+import luxe.Sprite;
+
+import phoenix.Color;
+import phoenix.Texture;
+import phoenix.Vector;
+
+class Main extends luxe.Game
+{
+
+    var hsp:Float;
+    var total:Int;
+    var twidth:Float;
+
+    var _offset:Float;
+
+    var circle:Texture;
+    var time:Float = 0;
+
+    override function config( config:AppConfig )
+    {
+        config.window.width = 700;
+        config.window.height = 600;
+
+        return config;
+
+    } //config
+
+
+    override public function ready():Void
+    {
+        // circle = new Texture();
+
+
+        hsp = 4;
+        total = 150;
+        twidth = (total-1) * hsp;
+
+
+        var noo:Ball;
+        for(i in 0...total)
+        {
+            _offset = Math.abs((total/2) - i);
+
+            noo = new Ball({
+                name: 'ball${i}',
+                //color: new Color().rgb(0xf94b04),
+                pos: new Vector( (Luxe.screen.w - twidth)/2 + hsp*i, Luxe.screen.h/2 ),
+                size: new Vector( Ball.genminScale(), Ball.genminScale() )
+            });
+            noo.generate({
+                offset: _offset
+            });
+            
+        }
+    }
+
+    override public function update(dt:Float):Void
+    {
+        // time += dt;
+        // if(time >= 1)
+        // {
+        //     time -= 1;
+        // }
+        // circle.x = Math.sin(time)*40 + 40;
+    }
+
+
+}
